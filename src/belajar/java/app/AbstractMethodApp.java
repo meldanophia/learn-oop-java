1ae30f8388e691cfceef2c5b1a4ce544026824b1
ENSEVAL\melda.nophia <meldanophia@gmail.com>
2024-03-14 14:34:16 +0700

    Abstract Method

---
+++ src/belajar/java/app/AbstractMethodApp.java
@@ -0,0 +1,16 @@
+package belajar.java.app;
+
+import belajar.java.data.Animal;
+import belajar.java.data.Cat;
+
+public class AbstractMethodApp {
+    public static void main(String[] args) {
+//        Cat cat = new Cat();
+//        cat.name = "Bobby";
+//        cat.run();
+
+        Animal enimel = new Cat();
+        enimel.name = "Bobby";
+        enimel.run();
+    }
+}
---
+++ src/belajar/java/data/Animal.java
@@ -0,0 +1,6 @@
+package belajar.java.data;
+
+public abstract class Animal {
+    public String name;
+    public abstract void run();
+}
