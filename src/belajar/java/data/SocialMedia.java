b9051e94c63fb93625c3801420083325488a5032
ENSEVAL\melda.nophia <meldanophia@gmail.com>
2024-03-20 16:45:17 +0700

    Final Class
    final class is the latest class and you cannot extends it to another class. Because , the final class couldn't have any child class anymore.

---
+++ src/belajar/java/data/SocialMedia.java
@@ -0,0 +1,13 @@
+package belajar.java.data;
+
+class SocialMedia {
+    String name;
+}
+
+final class Facebook extends SocialMedia{
+
+}
+
+//class FakeFacebook extends Facebook{
+//    // you cannot extends a final class, because the final class couldn't have anymore child class
+//}
