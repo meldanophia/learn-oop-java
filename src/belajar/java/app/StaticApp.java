08dd63d9b5c8afaec73fa69eeb6568ab049016ce
meldanophia <meldanophia@gmail.com>
2024-03-25 20:59:28 +0700

    Static Import
    we could use 2 way to import any static things

--- src/belajar/java/app/StaticApp.java
+++ src/belajar/java/app/StaticApp.java
@@ -1,14 +1,19 @@
 package belajar.java.app;
 
-import belajar.java.data.Appication;
-import belajar.java.data.Constant;
+//import belajar.java.data.Appication;
+import static belajar.java.data.Appication.PROCESSORS;
+//import belajar.java.data.Constant;
+import static belajar.java.data.Constant.*;
 import belajar.java.data.Country;
 import belajar.java.util.MathUtil;
 
 public class StaticApp {
     public static void main(String[] args) {
-        System.out.println(Constant.APPLICATION);
-        System.out.println(Constant.VERSION);
+//        System.out.println(Constant.APPLICATION);
+//        System.out.println(Constant.VERSION);
+
+        System.out.println(APPLICATION);
+        System.out.println(VERSION);
 
         System.out.println(MathUtil.sum(1,1,1,1,1));
 
@@ -17,6 +22,7 @@ public class StaticApp {
 
         System.out.println(city.getName());
 
-        System.out.println(Appication.PROCESSORS);
+//        System.out.println(Appication.PROCESSORS);
+        System.out.println(PROCESSORS);
     }
 }
