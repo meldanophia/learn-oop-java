a7606f2ce5b4bffe44b3fed04cca284f11726daa
meldanophia <meldanophia@gmail.com>
2024-03-29 20:10:20 +0700

    Enum Class
    enum just like a Constant at javascript

---
+++ src/belajar/java/app/EnumApp.java
@@ -0,0 +1,15 @@
+package belajar.java.app;
+
+import belajar.java.data.Customer;
+import belajar.java.data.Level;
+
+public class EnumApp {
+    public static void main(String[] args) {
+        Customer customer = new Customer();
+        customer.setName("Melda");
+        customer.setLevel(Level.PREMIUM);
+
+        System.out.println(customer.getName());
+        System.out.println(customer.getLevel());
+    }
+}
---
+++ src/belajar/java/data/Level.java
@@ -0,0 +1,10 @@
+package belajar.java.data;
+
+public enum Level {
+    //kebiasaannya pake huruf capslock
+    STANDARD,
+    PREMIUM,
+    VIP
+
+    //ttep bisa bikin method dibawah
+}
