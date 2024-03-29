245418e58914dc57093b7339bb1639f50e4019aa
meldanophia <meldanophia@gmail.com>
2024-03-29 20:18:56 +0700

    Enum Conversion
    Enum to String, String to Enum and print each of enum

--- src/belajar/java/app/EnumApp.java
+++ src/belajar/java/app/EnumApp.java
@@ -12,5 +12,16 @@ public class EnumApp {
         System.out.println(customer.getName());
         System.out.println(customer.getLevel());
         System.out.println(customer.getLevel().getDescription());
+
+        String levelName = Level.VIP.name();
+        System.out.println(levelName);
+
+        Level level = Level.valueOf("PREMIUM");
+        System.out.println(level);
+
+        System.out.println("Print Level");
+        for (var value: Level.values()){
+            System.out.println(value);
+        }
     }
 }
