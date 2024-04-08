f6f9ae4f2ce66908bfc169274525c3ae457399b8
ENSEVAL\melda.nophia <meldanophia@gmail.com>
2024-04-08 14:29:16 +0700

    Annotation
    Create a annotation that usually used for library/framework

--- src/belajar/java/app/CarApplication.java
+++ src/belajar/java/app/CarApplication.java
@@ -1,9 +1,10 @@
 package belajar.java.app;
 
+import belajar.java.annotation.Fancy;
 import belajar.java.data.Avanza;
 import belajar.java.data.Car;
 
-
+@Fancy(name = "ValidApp", tags = {"application", "java"})
 public class CarApplication {
     public static void main(String[] args) {
         Car car = new Avanza();
--- src/belajar/java/app/ValidationApp.java
+++ src/belajar/java/app/ValidationApp.java
@@ -1,9 +1,11 @@
 package belajar.java.app;
 
+import belajar.java.annotation.Fancy;
 import belajar.java.data.LoginRequest;
 import belajar.java.util.ValidationUtil;
 
 public class ValidationApp {
+//    @Fancy(name = "ValidApp", tags = {"application", "java"})
     public static void main(String[] args) {
 //        LoginRequest loginRequest = new LoginRequest("melda","Rahasia123");
 
